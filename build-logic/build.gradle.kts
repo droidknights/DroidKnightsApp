8fb907aad22d4f32c23ae5bb49d5c176d5fb1550
Youngjik Yoon <laco2951@gmail.com>
2023-06-04 20:39:23 +0900

    Hilt 재사용을 위한 플러그인 추가

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -14,5 +14,9 @@ gradlePlugin {
             id = "droidknights.android.compose"
             implementationClass = "com.droidknights.app2023.plugin.AndroidComposePlugin"
         }
+        register("androidHilt") {
+            id = "droidknights.android.hilt"
+            implementationClass = "com.droidknights.app2023.plugin.AndroidHiltPlugin"
+        }
     }
 }
