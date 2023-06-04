dceb9cb2b80b3a943384a3c147518eac240de2af
Youngjik Yoon <laco2951@gmail.com>
2023-06-04 19:23:26 +0900

    feature 모듈의 컨벤션 스크립트 추가

---
+++ build-logic/src/main/kotlin/droidknights.android.feature.gradle.kts
@@ -0,0 +1,9 @@
+import com.droidknights.app2023.configureCompose
+import com.droidknights.app2023.configureHiltAndroid
+
+plugins {
+    id("droidknights.android.library")
+}
+
+configureHiltAndroid()
+configureCompose()
