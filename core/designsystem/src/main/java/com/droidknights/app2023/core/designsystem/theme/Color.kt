b327beb22f29625b96102596cf44a2332981c1ec
wisemuji <wisemuji@gmail.com>
2023-06-26 21:10:55 +0900

    feat: 디자인 시스템 Color 정의 추가

--- core/designsystem/src/main/java/com/droidknights/app2023/core/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/droidknights/app2023/core/designsystem/theme/Color.kt
@@ -2,10 +2,33 @@ package com.droidknights.app2023.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val White = Color(0xFFFFFFFF)
+val PaleGray = Color(0xFFF9F9F9)
+val LightGray = Color(0xFFDCDCDC)
+val Gray = Color(0xFFC2C2C2)
+val DuskGray = Color(0xFF868686)
+val DarkGray = Color(0xFF5E5E5E)
+val Black = Color(0xFF000000)
+val Graphite = Color(0xFF292929)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple01 = Color(0xFFB469FF)
+val Mint01 = Color(0xFF7BFAF2)
+
+val Neon01 = Color(0xFF49F300)
+val Neon01A50 = Color(0x8049F300)
+val Neon02 = Color(0xFF6CFF2D)
+val Neon03 = Color(0xFF94FB67)
+val Neon04 = Color(0xFFA1FF78)
+val Neon05 = Color(0xFFEEFFE7)
+val Neon05A40 = Color(0x66EEFFE7)
+
+val Green01 = Color(0xFF88F21D)
+val Green02 = Color(0xFF6BEB33)
+val Green03 = Color(0xFF52C520)
+val Green03A30 = Color(0x4D52C520)
+val Green04 = Color(0xFF386524)
+
+val Yellow01 = Color(0xFFF2E800)
+val Yellow02 = Color(0xFFE8ED00)
+val Yellow03 = Color(0xFFBEED00)
+val Yellow03A40 = Color(0x66BEED00)
