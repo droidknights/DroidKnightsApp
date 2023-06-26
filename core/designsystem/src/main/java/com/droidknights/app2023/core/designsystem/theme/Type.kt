2cb7ff1dac3a2dd59b1a1f1e41a26eb98a862ab7
wisemuji <wisemuji@gmail.com>
2023-06-26 21:11:04 +0900

    feat: 디자인 시스템 Type 정의 추가

--- core/designsystem/src/main/java/com/droidknights/app2023/core/designsystem/theme/Type.kt
+++ core/designsystem/src/main/java/com/droidknights/app2023/core/designsystem/theme/Type.kt
@@ -6,30 +6,104 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
+private val SansSerifStyle = TextStyle(
+    fontFamily = FontFamily.SansSerif,
+    fontWeight = FontWeight.Normal,
+)
+
+private val DisplayLarge = SansSerifStyle.copy(
+    fontSize = 57.sp,
+    lineHeight = 64.sp,
+    letterSpacing = (-0.25).sp
+)
+
+private val DisplayMedium = SansSerifStyle.copy(
+    fontSize = 45.sp,
+    lineHeight = 52.sp,
+)
+
+private val DisplaySmall = SansSerifStyle.copy(
+    fontSize = 36.sp,
+    lineHeight = 44.sp,
+)
+
+private val HeadlineLarge = SansSerifStyle.copy(
+    fontSize = 32.sp,
+    lineHeight = 40.sp,
+)
+
+private val HeadlineMedium = SansSerifStyle.copy(
+    fontSize = 28.sp,
+    lineHeight = 36.sp,
+)
+
+private val HeadlineSmall = SansSerifStyle.copy(
+    fontSize = 24.sp,
+    lineHeight = 32.sp,
+)
+
+private val TitleLarge = SansSerifStyle.copy(
+    fontSize = 22.sp,
+    lineHeight = 28.sp,
+)
+
+private val TitleMedium = SansSerifStyle.copy(
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+)
+
+private val TitleSmall = SansSerifStyle.copy(
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+)
+
+private val LabelLarge = SansSerifStyle.copy(
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+)
+
+private val LabelMedium = SansSerifStyle.copy(
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+)
+
+private val LabelSmall = SansSerifStyle.copy(
+    fontSize = 11.sp,
+    lineHeight = 16.sp,
+)
+
+private val BodyLarge = SansSerifStyle.copy(
+    fontSize = 16.sp,
+    lineHeight = 24.sp,
+    letterSpacing = 0.5.sp
+)
+
+private val BodyMedium = SansSerifStyle.copy(
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = 0.25.sp
+)
+
+private val BodySmall = SansSerifStyle.copy(
+    fontSize = 12.sp,
+    lineHeight = 16.sp,
+    letterSpacing = 0.4.sp
+)
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-/*
-    Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-*/
+    displayLarge = DisplayLarge,
+    displayMedium = DisplayMedium,
+    displaySmall = DisplaySmall,
+    headlineLarge = HeadlineLarge,
+    headlineMedium = HeadlineMedium,
+    headlineSmall = HeadlineSmall,
+    titleLarge = TitleLarge,
+    titleMedium = TitleMedium,
+    titleSmall = TitleSmall,
+    labelLarge = LabelLarge,
+    labelMedium = LabelMedium,
+    labelSmall = LabelSmall,
+    bodyLarge = BodyLarge,
+    bodyMedium = BodyMedium,
+    bodySmall = BodySmall,
 )
