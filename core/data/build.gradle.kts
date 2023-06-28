cddb03cf6df4cb851c802ce56b6300cc5f7324d4
Youngjik Yoon <laco2951@gmail.com>
2023-06-28 22:00:41 +0900

    #11 data 모듈 Retrofit, Okhttp 의존성 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -2,3 +2,9 @@ plugins {
     id("droidknights.kotlin.library")
     id("droidknights.kotlin.hilt")
 }
+
+dependencies {
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.kotlin.serialization)
+    implementation(libs.okhttp.logging)
+}
