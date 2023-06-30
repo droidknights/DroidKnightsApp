e44690f9f531ac1249d21bdba24a28797bb115ac
Youngjik Yoon <laco2951@gmail.com>
2023-06-30 22:19:32 +0900

    #11 KotlinSerialization ConverterFactory 추가

--- core/data/src/main/java/com/droidknights/app2023/core/data/di/ApiModule.kt
+++ core/data/src/main/java/com/droidknights/app2023/core/data/di/ApiModule.kt
@@ -1,9 +1,12 @@
 package com.droidknights.app2023.core.data.di
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import javax.inject.Singleton
@@ -20,8 +23,18 @@ internal object ApiModule {
     
     @Provides
     @Singleton
-    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
+    fun provideRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json,
+    ): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .client(okHttpClient)
         .build()
+    
+    @Provides
+    @Singleton
+    fun provideJson(): Json = Json {
+        ignoreUnknownKeys = true
+    }
 }
