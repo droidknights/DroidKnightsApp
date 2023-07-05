9d4e066fe9e3724e57e1064eb91e00d2af88f744
Youngjik Yoon <laco2951@gmail.com>
2023-07-05 21:57:45 +0900

    #15 androidx core 및 lifecycle 라이브러리 의존성 추가

--- feature/main/build.gradle.kts
+++ feature/main/build.gradle.kts
@@ -5,3 +5,10 @@ plugins {
 android {
     namespace = "com.droidknights.app2023.feature.main"
 }
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+}
