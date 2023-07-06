5bdfc5d97c0de1ddecd9394ba8cf9c122535b25d
Youngjik Yoon <laco2951@gmail.com>
2023-07-06 23:03:55 +0900

    #21 Scaffold 추가

--- feature/main/src/main/java/com/droidknights/app2023/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/droidknights/app2023/feature/main/MainScreen.kt
@@ -1,8 +1,30 @@
 package com.droidknights.app2023.feature.main
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun MainScreen() {
-
+    Scaffold(
+        content = { padding ->
+            // TODO: Background color using scheme
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFFF9F9F9))
+                    .padding(padding)
+            ) {
+            
+            }
+        },
+        bottomBar = {
+            // TODO: Composable with Bottom Bar
+        },
+    )
 }
