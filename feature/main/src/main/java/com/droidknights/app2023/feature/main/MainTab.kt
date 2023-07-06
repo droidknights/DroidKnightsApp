fd1f93e4e856db14b02358b4d40cd12a74299799
Youngjik Yoon <laco2951@gmail.com>
2023-07-06 23:54:53 +0900

    #21 MainTab enum 타입 종류 추가

---
+++ feature/main/src/main/java/com/droidknights/app2023/feature/main/MainTab.kt
@@ -0,0 +1,9 @@
+package com.droidknights.app2023.feature.main
+
+enum class MainTab(
+    val iconResId: Int,
+) {
+    SETTING(R.drawable.ic_setting),
+    HOME(R.drawable.ic_home),
+    TEMP(R.drawable.ic_temp),
+}
