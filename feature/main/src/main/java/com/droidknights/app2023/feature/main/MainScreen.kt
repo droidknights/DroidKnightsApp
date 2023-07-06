3512ae6aa6386e64b1d1f9e0092ebcf0e8840f94
Youngjik Yoon <laco2951@gmail.com>
2023-07-06 23:26:06 +0900

    #21 BottomBar 컴포저블 추가

--- feature/main/src/main/java/com/droidknights/app2023/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/droidknights/app2023/feature/main/MainScreen.kt
@@ -1,19 +1,25 @@
 package com.droidknights.app2023.feature.main
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainScreen() {
     Scaffold(
         content = { padding ->
-            // TODO: Background color using scheme
             Box(
                 modifier = Modifier
                     .fillMaxSize()
@@ -24,7 +30,26 @@ fun MainScreen() {
             }
         },
         bottomBar = {
-            // TODO: Composable with Bottom Bar
+            MainBottomBar()
         },
     )
+}
+
+@Composable
+private fun MainBottomBar(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .navigationBarsPadding()
+            .padding(start = 8.dp, end = 8.dp, bottom = 28.dp)
+            .fillMaxWidth()
+            .height(56.dp)
+            .border(
+                width = 1.dp,
+                color = Color(0xFFDCDCDC), // lightgray
+                shape = RoundedCornerShape(size = 28.dp)
+            )
+            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(28.dp)),
+    ) {
+        // TODO: BottomBar Item
+    }
 }
