8fc7df297090f601192f9c7df1693cbb9f47d8ba
Youngjik Yoon <laco2951@gmail.com>
2023-07-06 22:39:10 +0900

    #21 메인 액티비티를 투명 상태바로 변경

--- feature/main/src/main/java/com/droidknights/app2023/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/droidknights/app2023/feature/main/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         
         setContent {
             MaterialTheme {
