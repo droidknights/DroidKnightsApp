786a660dd8038e6b1df816d0f818c36f98d46ae7
Youngjik Yoon <laco2951@gmail.com>
2023-07-06 23:55:16 +0900

    #21 MainBottomBar 컴포저블 구현

--- feature/main/src/main/java/com/droidknights/app2023/feature/main/MainScreen.kt
+++ feature/main/src/main/java/com/droidknights/app2023/feature/main/MainScreen.kt
@@ -2,22 +2,35 @@ package com.droidknights.app2023.feature.main
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MainScreen() {
+    var currentTab by remember { mutableStateOf(MainTab.HOME) }
     Scaffold(
         content = { padding ->
             Box(
@@ -26,17 +39,26 @@ fun MainScreen() {
                     .background(Color(0xFFF9F9F9))
                     .padding(padding)
             ) {
-            
+                // TODO: 컨텐츠 추가
             }
         },
         bottomBar = {
-            MainBottomBar()
+            MainBottomBar(
+                tabs = MainTab.values().toList(),
+                currentTab = currentTab,
+                onTabSelected = { currentTab = it }
+            )
         },
     )
 }
 
 @Composable
-private fun MainBottomBar(modifier: Modifier = Modifier) {
+private fun MainBottomBar(
+    tabs: List<MainTab>,
+    currentTab: MainTab,
+    onTabSelected: (MainTab) -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Row(
         modifier = modifier
             .navigationBarsPadding()
@@ -48,8 +70,45 @@ private fun MainBottomBar(modifier: Modifier = Modifier) {
                 color = Color(0xFFDCDCDC), // lightgray
                 shape = RoundedCornerShape(size = 28.dp)
             )
-            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(28.dp)),
+            .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(28.dp))
+            .padding(horizontal = 28.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        // TODO: BottomBar Item
+        tabs.forEach { tab ->
+            MainBottomBarItem(
+                selected = tab == currentTab,
+                onClick = { onTabSelected(tab) },
+                resId = tab.iconResId,
+            )
+        }
+    }
+}
+
+@Composable
+private fun RowScope.MainBottomBarItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    resId: Int,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .selectable(
+                selected = selected,
+                indication = null,
+                role = null,
+                interactionSource = remember { MutableInteractionSource() },
+                onClick = onClick,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            painter = painterResource(id = resId),
+            contentDescription = null,
+            tint = if (selected) Color(0xFF49F300) else Color(0xFFDCDCDC)
+        )
     }
 }
