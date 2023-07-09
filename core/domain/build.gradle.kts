6c30b2458ad3c5bbae90b169e6c924056249e413
wisemuji <wisemuji@gmail.com>
2023-07-10 00:42:09 +0900

    #33 domain 모듈을 data 모듈을 의존하는 android library로 변경

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
-    id("droidknights.kotlin.library")
+    id("droidknights.android.library")
+}
+
+android {
+    namespace = "com.droidknights.app2023.core.domain"
 }
 
 dependencies {
+    implementation(project(":core:data"))
+
     implementation(libs.inject)
 }
