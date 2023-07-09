12a0570ea01057ef0a53a3e286128e95dcc8710d
wisemuji <wisemuji@gmail.com>
2023-07-10 00:53:37 +0900

    #33 ContributorViewModel로 컨트리뷰터 조회 상태 노출

--- feature/contributor/build.gradle.kts
+++ feature/contributor/build.gradle.kts
@@ -5,3 +5,11 @@ plugins {
 android {
     namespace = "com.droidknights.app2023.feature.contributor"
 }
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+    implementation(libs.hilt.navigation.compose)
+}
