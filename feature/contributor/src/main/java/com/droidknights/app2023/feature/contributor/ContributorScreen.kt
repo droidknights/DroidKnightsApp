75afd73f79ba87e987c4f20e7d85cd5d3ae20fa5
wisemuji <wisemuji@gmail.com>
2023-07-10 01:02:07 +0900

    #25 컨트리뷰터 화면 ViewModel 연결

--- feature/contributor/src/main/java/com/droidknights/app2023/feature/contributor/ContributorScreen.kt
+++ feature/contributor/src/main/java/com/droidknights/app2023/feature/contributor/ContributorScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,8 +21,22 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.droidknights.app2023.core.domain.contributor.Contributor
 
+@Composable
+fun ContributorRoute(
+    modifier: Modifier = Modifier,
+    viewModel: ContributorViewModel = hiltViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    ContributorScreen(
+        uiState = uiState,
+        modifier = modifier,
+    )
+}
 
 @Composable
 internal fun ContributorScreen(
