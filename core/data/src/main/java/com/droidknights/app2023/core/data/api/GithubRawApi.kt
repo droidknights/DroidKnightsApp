4b997ecc949303f96e1efeb97c63be01072b2fea
Youngjik Yoon <laco2951@gmail.com>
2023-07-16 16:43:57 +0900

    #50 /sponsors API 인터페이스 추가

--- core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
+++ core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
@@ -1,3 +1,10 @@
 package com.droidknights.app2023.core.data.api
 
-internal interface GithubRawApi
+import com.droidknights.app2023.core.data.api.model.SponsorResponse
+import retrofit2.http.GET
+
+internal interface GithubRawApi {
+    
+    @GET("/droidknights/DroidKnights2023_App/main/data/src/main/assets/sponsors.json")
+    suspend fun getSponsors(): List<SponsorResponse>
+}
