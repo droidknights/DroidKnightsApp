1f42811fe229b72c44c6960c5e9fefd0e1c9968f
Youngjik Yoon <laco2951@gmail.com>
2023-07-16 14:07:54 +0900

    #37 메인 액티비티가 KnightsTheme 사용하도록 변경

--- feature/main/src/main/java/com/droidknights/app2023/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/droidknights/app2023/feature/main/MainActivity.kt
@@ -3,8 +3,8 @@ package com.droidknights.app2023.feature.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
 import androidx.core.view.WindowCompat
+import com.droidknights.app2023.core.designsystem.theme.KnightsTheme
 import com.droidknights.app2023.core.navigation.HomeNavigation
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         
         setContent {
-            MaterialTheme {
+            KnightsTheme {
                 MainScreen(homeNavigation)
             }
         }
