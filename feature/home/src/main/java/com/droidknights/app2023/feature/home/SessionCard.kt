9ce4631ec730d6302a7568bf55d02925583e233b
wisemuji <wisemuji@gmail.com>
2023-07-19 00:14:26 +0900

    #36 세션 카드 UI 추가

---
+++ feature/home/src/main/java/com/droidknights/app2023/feature/home/SessionCard.kt
@@ -0,0 +1,82 @@
+package com.droidknights.app2023.feature.home
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.droidknights.app2023.core.designsystem.theme.KnightsTheme
+
+@Composable
+internal fun SessionCard(
+    onClick: () -> Unit,
+) {
+    Surface(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(164.dp),
+        shape = RoundedCornerShape(32.dp),
+        shadowElevation = 2.dp,
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_card_session),
+            contentScale = ContentScale.FillBounds,
+            contentDescription = null
+        )
+        Column(
+            modifier = Modifier
+                .padding(vertical = 16.dp, horizontal = 24.dp)
+        ) {
+            SessionCardCaption()
+            Text(
+                text = stringResource(id = R.string.session_card_title),
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.Black,
+                color = Color(0xFF000000),
+                modifier = Modifier.padding(top = 12.dp),
+            )
+        }
+    }
+}
+
+@Composable
+private fun SessionCardCaption() {
+    Box(
+        modifier = Modifier
+            .background(Color(0xFF292929), RoundedCornerShape(32.dp))
+            .padding(horizontal = 12.dp, vertical = 2.dp)
+    ) {
+        Text(
+            text = stringResource(id = R.string.session_card_caption),
+            style = MaterialTheme.typography.labelSmall,
+            fontWeight = FontWeight.Medium,
+            color = Color(0xFFFFFFFF),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SessionCardPreview() {
+    KnightsTheme {
+        SessionCard(
+            onClick = { }
+        )
+    }
+}
