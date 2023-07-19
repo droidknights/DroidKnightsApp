d5ad245cd8465e9f8c339920c96a53088bdedd5e
Youngjik Yoon <laco2951@gmail.com>
2023-07-19 22:18:08 +0900

    #55 사용하지 않는 코드 제거

--- core/navigation/src/main/java/com/droidknights/app2023/core/navigation/MainDestination.kt
+++
@@ -1,9 +0,0 @@
-package com.droidknights.app2023.core.navigation
-
-sealed class MainDestination(val route: String) {
-    object Home : MainDestination("home")
-    
-    object Setting : MainDestination("setting")
-    
-    object Temp : MainDestination("temp")
-}
