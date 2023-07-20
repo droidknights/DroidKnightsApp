e1417e6e07b405f2c62965977bfe0d61a1b18a91
wisemuji <wisemuji@gmail.com>
2023-07-20 21:38:56 +0900

    #45 HomeScreen에서 padding값 세팅

--- feature/home/src/main/java/com/droidknights/app2023/feature/home/HomeScreen.kt
+++ feature/home/src/main/java/com/droidknights/app2023/feature/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.droidknights.app2023.feature.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,10 +11,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
-internal fun HomeScreen(onContributorClick: () -> Unit) {
+internal fun HomeScreen(padding: PaddingValues, onContributorClick: () -> Unit) {
     Column(
         Modifier
             .background(color = Color(0xFFF9F9F9))
+            .padding(padding)
             .padding(horizontal = 8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
--- feature/home/src/main/java/com/droidknights/app2023/feature/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/java/com/droidknights/app2023/feature/home/navigation/HomeNavigation.kt
@@ -1,12 +1,6 @@
 package com.droidknights.app2023.feature.home.navigation
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
@@ -21,14 +15,7 @@ fun NavGraphBuilder.homeNavGraph(
     onContributorClick: () -> Unit,
 ) {
     composable(route = HomeRoute.route) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color(0xFFF9F9F9))
-                .padding(padding)
-        ) {
-            HomeScreen(onContributorClick)
-        }
+        HomeScreen(padding, onContributorClick)
     }
 }
 
