f1e219fd84b7bb8a6692e8a971a937d24b7d91db
wisemuji <wisemuji@gmail.com>
2023-07-22 00:40:58 +0900

    #44 Session Github Raw API 추가

--- core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
+++ core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
@@ -1,5 +1,6 @@
 package com.droidknights.app2023.core.data.api
 
+import com.droidknights.app2023.core.data.api.model.SessionResponse
 import com.droidknights.app2023.core.data.api.model.SponsorResponse
 import retrofit2.http.GET
 
@@ -7,4 +8,7 @@ internal interface GithubRawApi {
     
     @GET("/droidknights/DroidKnights2023_App/main/data/src/main/assets/sponsors.json")
     suspend fun getSponsors(): List<SponsorResponse>
+
+    @GET("/droidknights/DroidKnights2023_App/main/data/src/main/assets/sponsors.json")
+    suspend fun getSessions(): List<SessionResponse>
 }
---
+++ core/data/src/main/java/com/droidknights/app2023/core/data/api/model/SessionResponse.kt
@@ -0,0 +1,20 @@
+package com.droidknights.app2023.core.data.api.model
+
+import com.droidknights.app2023.core.model.Level
+import com.droidknights.app2023.core.model.Room
+import com.droidknights.app2023.core.model.Speaker
+import com.droidknights.app2023.core.model.Tag
+import kotlinx.datetime.LocalDateTime
+import kotlinx.serialization.Serializable
+
+@Serializable
+internal data class SessionResponse(
+    val title: String,
+    val content: List<String>,
+    val speakers: List<Speaker>,
+    val level: Level,
+    val tags: List<Tag> = emptyList(),
+    val room: Room = Room.TRACK1,
+    val startTime: LocalDateTime,
+    val endTime: LocalDateTime,
+)
