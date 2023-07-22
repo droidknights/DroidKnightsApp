ee075887b64e90a729483c3135577791d355362a
Youngjik Yoon <laco2951@gmail.com>
2023-07-22 16:35:27 +0900

    #54 불필요한 주석 제거

--- feature/main/src/main/java/com/droidknights/app2023/feature/main/MainNavigator.kt
+++ feature/main/src/main/java/com/droidknights/app2023/feature/main/MainNavigator.kt
@@ -33,16 +33,10 @@ internal class MainNavigator(
 
     fun navigate(tab: MainTab) {
         val navOptions = navOptions {
-            // Pop up to the start destination of the graph to
-            // avoid building up a large stack of destinations
-            // on the back stack as users select items
             popUpTo(navController.graph.findStartDestination().id) {
                 saveState = true
             }
-            // Avoid multiple copies of the same destination when
-            // reselecting the same item
             launchSingleTop = true
-            // Restore state when reselecting a previously selected item
             restoreState = true
         }
 
