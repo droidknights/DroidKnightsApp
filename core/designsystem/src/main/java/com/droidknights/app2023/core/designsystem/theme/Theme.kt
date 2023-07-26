bfb7c43c75039d2a9b315827c1f2e077b9a08638
Youngjik Yoon <laco2951@gmail.com>
2023-07-27 08:44:29 +0900

    #95 테마에 따라 상태바 색상 변경

--- core/designsystem/src/main/java/com/droidknights/app2023/core/designsystem/theme/Theme.kt
+++ core/designsystem/src/main/java/com/droidknights/app2023/core/designsystem/theme/Theme.kt
@@ -1,5 +1,6 @@
 package com.droidknights.app2023.core.designsystem.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
@@ -7,7 +8,11 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.platform.LocalInspectionMode
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = White,
@@ -80,12 +85,25 @@ val ColorScheme.tertiaryFixedDim get() = DuskGray
 val ColorScheme.onTertiaryFixed get() = Yellow01
 val ColorScheme.onTertiaryFixedVariant get() = LightGray
 
+val ColorScheme.surfaceDim
+    @Composable
+    get() = if (LocalDarkTheme.current) Black else PaleGray
+
 @Composable
 fun KnightsTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit,
 ) {
     val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    if (!LocalInspectionMode.current) {
+        val view = LocalView.current
+        SideEffect {
+            val window = (view.context as Activity).window
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
+    }
+
     CompositionLocalProvider(
         LocalDarkTheme provides darkTheme,
         LocalTypography provides Typography
