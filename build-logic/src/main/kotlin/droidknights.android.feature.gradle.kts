0b497fa4d5032167c4127a2a870ff50218873436
Youngjik Yoon <laco2951@gmail.com>
2023-07-26 22:43:52 +0900

    #108 featrue 모듈이 core-ui 모듈을 의존하도록 한다

--- build-logic/src/main/kotlin/droidknights.android.feature.gradle.kts
+++ build-logic/src/main/kotlin/droidknights.android.feature.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(project(":core:designsystem"))
     implementation(project(":core:domain"))
     implementation(project(":core:navigation"))
+    implementation(project(":core:ui"))
     
     val libs = project.extensions.libs
     implementation(libs.findLibrary("hilt.navigation.compose").get())
