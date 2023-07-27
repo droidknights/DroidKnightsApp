3b45a13f4dc09b62b3973ecc91fa4e071d72a579
wisemuji <wisemuji@gmail.com>
2023-07-28 02:02:40 +0900

    #72 후원사 카드 LazyRow로 변경 및 패딩값 조절

--- feature/home/src/main/java/com/droidknights/app2023/feature/home/SponsorCard.kt
+++ feature/home/src/main/java/com/droidknights/app2023/feature/home/SponsorCard.kt
@@ -1,18 +1,25 @@
 package com.droidknights.app2023.feature.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.MutatePriority
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -36,45 +43,48 @@ fun SponsorCard(
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(horizontal = 24.dp)
         ) {
-            Text(
-                text = stringResource(id = R.string.sponsor_card_title),
-                style = KnightsTheme.typography.headlineSmallBL,
-                color = Color(0xFF000000),
-                modifier = Modifier.padding(top = 24.dp),
-            )
-            Text(
-                text = stringResource(
-                    id = R.string.sponsor_card_description,
-                    uiState.platinumCount,
-                    uiState.goldCount
-                ),
-                style = KnightsTheme.typography.titleSmallR,
-                color = Color(0xFF868686),
-                modifier = Modifier.padding(top = 8.dp),
-            )
+            Column(modifier = Modifier.padding(horizontal = 24.dp)) {
+                Text(
+                    text = stringResource(id = R.string.sponsor_card_title),
+                    style = KnightsTheme.typography.headlineSmallBL,
+                    color = Color(0xFF000000),
+                    modifier = Modifier.padding(top = 24.dp),
+                )
+                Text(
+                    text = stringResource(
+                        id = R.string.sponsor_card_description,
+                        uiState.platinumCount,
+                        uiState.goldCount
+                    ),
+                    style = KnightsTheme.typography.titleSmallR,
+                    color = Color(0xFF868686),
+                    modifier = Modifier.padding(top = 8.dp),
+                )
+            }
             SponsorGroup(uiState.sponsors)
         }
     }
 }
 
-// TODO: 이미지 연결 및 동적으로 배치
+// TODO: 세로 두 줄 고정 필요
 @Composable
 private fun SponsorGroup(
     sponsors: List<Sponsor>,
 ) {
-    Row(
+    val itemsListState = remember { sponsors.toMutableStateList() }
+
+    LazyRow(
         horizontalArrangement = Arrangement
             .spacedBy(
-                space = 8.dp,
+                space = 14.dp,
                 alignment = Alignment.CenterHorizontally,
             ),
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(24.dp),
+            .padding(vertical = 24.dp)
+            .fillMaxWidth(),
     ) {
-        sponsors.forEach {
+        items(itemsListState, key = { it.name }) {
             SponsorLogo(it)
         }
     }
