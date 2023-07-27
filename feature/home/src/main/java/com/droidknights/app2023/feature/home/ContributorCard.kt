25af818bc34b59a23dd86f56a07f4d69323ce042
Youngjik Yoon <laco2951@gmail.com>
2023-07-27 23:33:43 +0900

    #125 고정 색상을 이용해 카드 변경

--- feature/home/src/main/java/com/droidknights/app2023/feature/home/ContributorCard.kt
+++ feature/home/src/main/java/com/droidknights/app2023/feature/home/ContributorCard.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -17,6 +16,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.droidknights.app2023.core.designsystem.component.KnightsCard
+import com.droidknights.app2023.core.designsystem.theme.Black
+import com.droidknights.app2023.core.designsystem.theme.Green03
 import com.droidknights.app2023.core.designsystem.theme.KnightsTheme
 
 @Composable
@@ -24,10 +25,9 @@ internal fun ContributorCard(
     onClick: () -> Unit,
 ) {
     KnightsCard(
-        color = Color(0xFFEEFFE7),
+        color = CardBackgroundColor,
         onClick = onClick,
-        modifier = Modifier
-            .height(164.dp),
+        modifier = Modifier.height(164.dp),
     ) {
         Box(modifier = Modifier.padding(horizontal = 24.dp)) {
             Image(
@@ -46,19 +46,21 @@ internal fun ContributorCard(
             Text(
                 text = stringResource(id = R.string.contributor_card_title),
                 style = KnightsTheme.typography.headlineSmallBL,
-                color = Color(0xFF000000),
+                color = Black,
                 modifier = Modifier.padding(top = 24.dp),
             )
 
             Text(
                 text = stringResource(id = R.string.contributor_card_description),
                 style = KnightsTheme.typography.titleSmallR,
-                color = Color(0xFF52C520),
+                color = Green03,
                 modifier = Modifier.padding(top = 6.dp),
             )
         }
     }
 }
+
+private val CardBackgroundColor = Color(0xFFEEFFE7)
 
 @Preview
 @Composable
