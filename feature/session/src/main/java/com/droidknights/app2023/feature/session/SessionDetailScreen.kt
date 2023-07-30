260f2bb1e6748b9e7abe3d5b7290937b3217e483
Youngjik Yoon <laco2951@gmail.com>
2023-07-30 11:39:43 +0900

    #43 화면 진입시 뷰모델 데이터 요청

--- feature/session/src/main/java/com/droidknights/app2023/feature/session/SessionDetailScreen.kt
+++ feature/session/src/main/java/com/droidknights/app2023/feature/session/SessionDetailScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -33,6 +34,10 @@ internal fun SessionDetailScreen(
     ) {
         SessionDetailTopAppBar(onBackClick = onBackClick)
         SessionDetailContent(uiState = sessionUiState)
+    }
+
+    LaunchedEffect(sessionId) {
+        viewModel.fetchSession(sessionId)
     }
 }
 
