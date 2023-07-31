2f7119cd72d32ad239d8b446dc5ed425ef0ccadc
wisemuji <wisemuji@gmail.com>
2023-07-31 21:25:02 +0900

    #117 후원사 가짜 데이터 UseCase에서 전달

--- core/domain/src/main/java/com/droidknights/app2023/core/domain/usecase/GetSponsorsUseCase.kt
+++ core/domain/src/main/java/com/droidknights/app2023/core/domain/usecase/GetSponsorsUseCase.kt
@@ -8,7 +8,46 @@ class GetSponsorsUseCase @Inject constructor(
     private val sponsorRepository: SponsorRepository,
 ) {
 
+    // TODO: Sponsor 데이터 연결
+
     suspend operator fun invoke(): List<Sponsor> {
-        return sponsorRepository.getSponsors()
+        return listOf(
+            Sponsor(
+                name = "Sponsor1",
+                homepage = "https://www.instagram.com/droid_knights",
+                grade = Sponsor.Grade.GOLD,
+                imageUrl = "https://picsum.photos/id/237/200/200",
+            ),
+            Sponsor(
+                name = "Sponsor2",
+                homepage = "https://www.instagram.com/droid_knights",
+                grade = Sponsor.Grade.PLATINUM,
+                imageUrl = "https://picsum.photos/id/204/200/200",
+            ),
+            Sponsor(
+                name = "Sponsor3",
+                homepage = "https://www.instagram.com/droid_knights",
+                grade = Sponsor.Grade.PLATINUM,
+                imageUrl = "https://picsum.photos/id/203/200/200",
+            ),
+            Sponsor(
+                name = "Sponsor4",
+                homepage = "https://www.instagram.com/droid_knights",
+                grade = Sponsor.Grade.PLATINUM,
+                imageUrl = "https://picsum.photos/id/202/200/200",
+            ),
+            Sponsor(
+                name = "Sponsor5",
+                homepage = "https://www.instagram.com/droid_knights",
+                grade = Sponsor.Grade.PLATINUM,
+                imageUrl = "https://picsum.photos/id/201/200/200",
+            ),
+            Sponsor(
+                name = "Sponsor6",
+                homepage = "https://www.instagram.com/droid_knights",
+                grade = Sponsor.Grade.PLATINUM,
+                imageUrl = "https://picsum.photos/id/200/200/200",
+            ),
+        )
     }
 }
--- feature/home/src/main/java/com/droidknights/app2023/feature/home/SponsorCard.kt
+++ feature/home/src/main/java/com/droidknights/app2023/feature/home/SponsorCard.kt
@@ -39,48 +39,7 @@ private const val SCROLL_DELAY_MILLIS = 20L
 private const val SCROLL_PIXEL_UNIT = 4f
 
 @Composable
-fun SponsorCard(
-    // TODO: 실제 데이터와 연결
-    uiState: SponsorsUiState =
-        listOf(
-            Sponsor(
-                name = "Sponsor1",
-                homepage = "https://www.instagram.com/droid_knights",
-                grade = Sponsor.Grade.GOLD,
-                imageUrl = "https://picsum.photos/id/237/200/200",
-            ),
-            Sponsor(
-                name = "Sponsor2",
-                homepage = "https://www.instagram.com/droid_knights",
-                grade = Sponsor.Grade.PLATINUM,
-                imageUrl = "https://picsum.photos/id/204/200/200",
-            ),
-            Sponsor(
-                name = "Sponsor3",
-                homepage = "https://www.instagram.com/droid_knights",
-                grade = Sponsor.Grade.PLATINUM,
-                imageUrl = "https://picsum.photos/id/203/200/200",
-            ),
-            Sponsor(
-                name = "Sponsor4",
-                homepage = "https://www.instagram.com/droid_knights",
-                grade = Sponsor.Grade.PLATINUM,
-                imageUrl = "https://picsum.photos/id/202/200/200",
-            ),
-            Sponsor(
-                name = "Sponsor5",
-                homepage = "https://www.instagram.com/droid_knights",
-                grade = Sponsor.Grade.PLATINUM,
-                imageUrl = "https://picsum.photos/id/201/200/200",
-            ),
-            Sponsor(
-                name = "Sponsor6",
-                homepage = "https://www.instagram.com/droid_knights",
-                grade = Sponsor.Grade.PLATINUM,
-                imageUrl = "https://picsum.photos/id/200/200/200",
-            ),
-        ).let(::SponsorsUiState),
-) {
+fun SponsorCard(uiState: SponsorsUiState) {
     KnightsCard {
         Column(
             modifier = Modifier.fillMaxWidth()
