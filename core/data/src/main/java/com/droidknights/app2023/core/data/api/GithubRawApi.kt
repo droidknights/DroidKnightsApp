b0ce8a2151ae629d56b828584ae062a3ee224e8a
Youngjik Yoon <laco2951@gmail.com>
2023-08-16 22:38:28 +0900

    #208 잘못 설정되어 있는 json 경로 변경

--- core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
+++ core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
@@ -6,9 +6,9 @@ import retrofit2.http.GET
 
 internal interface GithubRawApi {
 
-    @GET("/droidknights/DroidKnights2023_App/main/data/src/main/assets/sponsors.json")
+    @GET("/droidknights/DroidKnights2023_App/main/core/data/src/main/assets/sponsors.json")
     suspend fun getSponsors(): List<SponsorResponse>
 
-    @GET("/droidknights/DroidKnights2023_App/main/data/src/main/assets/sessions.json")
+    @GET("/droidknights/DroidKnights2023_App/main/core/data/src/main/assets/sessions.json")
     suspend fun getSessions(): List<SessionResponse>
 }
