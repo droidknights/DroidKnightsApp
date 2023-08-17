52a0acb80e1d828d24686eb6ac0c2e1f7543cfa8
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-08-17 10:36:50 +0900

    [feature/#17] 코드리뷰 반영 - enableFeaturePreview 위치 조정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
         mavenCentral()
     }
 }
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
