da5ebd80f98031f4c872a4b52bb0172b2a2630ea
gowoonchoi <gowoonchoi@ncsoft.com>
2023-08-22 04:16:24 +0900

    fix: code conventions

--- feature/session/src/main/java/com/droidknights/app2023/feature/session/SessionDetailEffect.kt
+++ feature/session/src/main/java/com/droidknights/app2023/feature/session/SessionDetailEffect.kt
@@ -1,6 +1,6 @@
 package com.droidknights.app2023.feature.session
 
 sealed interface SessionDetailEffect {
-    object Idle: SessionDetailEffect
-    data class ShowToastForBookmarkState(val bookmarked: Boolean): SessionDetailEffect
+    object Idle : SessionDetailEffect
+    data class ShowToastForBookmarkState(val bookmarked: Boolean) : SessionDetailEffect
 }
