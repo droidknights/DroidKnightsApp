dce8d02a4d2c5bab4c5c95cd12cf858d92d166c0
workspace <ryukimin.workspace@gmail.com>
2023-08-31 21:18:40 +0900

    재생 중인 Session의 id를 datastore에 기록

---
+++ core/datastore/src/main/java/com/droidknights/app2023/core/datastore/PlaybackPreferencesDataSource.kt
@@ -0,0 +1,27 @@
+package com.droidknights.app2023.core.datastore
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.droidknights.app2023.core.datastore.model.PlaybackData
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class PlaybackPreferencesDataSource @Inject constructor(
+    private val dataStore: DataStore<Preferences>
+) {
+    object PreferencesKey {
+        val CURRENT_SESSION_ID = stringPreferencesKey("CURRENT_SESSION_ID")
+    }
+
+    val playbackData = dataStore.data.map { preferences ->
+        preferences[PreferencesKey.CURRENT_SESSION_ID]?.let(::PlaybackData)
+    }
+
+    suspend fun updateCurrentSessionId(sessionId: String) {
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.CURRENT_SESSION_ID] = sessionId
+        }
+    }
+}
---
+++ core/datastore/src/main/java/com/droidknights/app2023/core/datastore/model/PlaybackData.kt
@@ -0,0 +1,5 @@
+package com.droidknights.app2023.core.datastore.model
+
+data class PlaybackData(
+    val currentSessionId: String
+)
