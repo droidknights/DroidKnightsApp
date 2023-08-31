4b703da6e29cb3640aacdac0c00903640623846c
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-08-31 17:29:09 +0900

    [feature/#202] Apply detekt

--- core/data/src/main/java/com/droidknights/app2023/core/data/di/DataModule.kt
+++ core/data/src/main/java/com/droidknights/app2023/core/data/di/DataModule.kt
@@ -18,7 +18,6 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import javax.inject.Named
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
