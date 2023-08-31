5feda1e45768b5eb5e3e5430e4e9ec511b4b2b71
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-08-31 18:22:02 +0900

    [feature/#202] Fake DataSource 테스트 통과시키게 수정

--- core/data/src/test/java/com/droidknights/app2023/core/data/datastore/fake/FakeSessionPreferencesDataSource.kt
+++ core/data/src/test/java/com/droidknights/app2023/core/data/datastore/fake/FakeSessionPreferencesDataSource.kt
@@ -2,16 +2,14 @@ package com.droidknights.app2023.core.data.datastore.fake
 
 import com.droidknights.app2023.core.data.datasource.SessionLocalDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.filterNotNull
 
 class FakeSessionPreferencesDataSource : SessionLocalDataSource {
-    private val _bookmarkedSession = mutableSetOf<String>()
-    override val bookmarkedSession: Flow<Set<String>> = flow {
-        emit(_bookmarkedSession)
-    }
+    private val _bookmarkedSession = MutableStateFlow(emptySet<String>())
+    override val bookmarkedSession: Flow<Set<String>> = _bookmarkedSession.filterNotNull()
 
-    override suspend fun updateBookmarkedSession(newSession: Set<String>) {
-        _bookmarkedSession.clear()
-        _bookmarkedSession.addAll(newSession)
+    override suspend fun updateBookmarkedSession(bookmarkedSession: Set<String>) {
+        _bookmarkedSession.value = bookmarkedSession.toSet()
     }
 }
