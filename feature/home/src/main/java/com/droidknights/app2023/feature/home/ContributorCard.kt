cce12073936ccdb89b7a6802478708b0e915b630
KwonDae <green201402317@gmail.com>
2023-09-05 09:05:57 +0900

    #248 [홈] 사용하지 않는 Import 삭제

--- feature/home/src/main/java/com/droidknights/app2023/feature/home/ContributorCard.kt
+++ feature/home/src/main/java/com/droidknights/app2023/feature/home/ContributorCard.kt
@@ -3,13 +3,11 @@ package com.droidknights.app2023.feature.home
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
