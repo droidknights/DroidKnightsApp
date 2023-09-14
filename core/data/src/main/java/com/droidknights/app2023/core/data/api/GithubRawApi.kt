7d9204d899004c7b001fd1a4adb74721980fa591
workspace <ryukimin.workspace@gmail.com>
2023-09-14 17:43:19 +0900

    GithubRawApi url 변경

--- core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
+++ core/data/src/main/java/com/droidknights/app2023/core/data/api/GithubRawApi.kt
@@ -6,9 +6,9 @@ import retrofit2.http.GET
 
 internal interface GithubRawApi {
 
-    @GET("/workspace/DroidKnights2023_App/media3/core/data/src/main/assets/sponsors.json")
+    @GET("/droidknights/DroidKnights2023_App/reference-media3/core/data/src/main/assets/sponsors.json")
     suspend fun getSponsors(): List<SponsorResponse>
 
-    @GET("/workspace/DroidKnights2023_App/media3/core/data/src/main/assets/sessions.json")
+    @GET("/droidknights/DroidKnights2023_App/reference-media3/core/data/src/main/assets/sessions.json")
     suspend fun getSessions(): List<SessionResponse>
 }
