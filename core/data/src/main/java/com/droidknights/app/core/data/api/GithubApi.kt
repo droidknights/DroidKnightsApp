c0ab281eb6308e63bb8eb04bb4a97a085179cd73
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-05-29 00:53:04 +0900

    feat: Commit API, CommitResponse

--- core/data/src/main/java/com/droidknights/app/core/data/api/GithubApi.kt
+++ core/data/src/main/java/com/droidknights/app/core/data/api/GithubApi.kt
@@ -1,8 +1,10 @@
 package com.droidknights.app.core.data.api
 
+import com.droidknights.app.core.data.api.model.CommitResponse
 import com.droidknights.app.core.data.api.model.ContributorResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 internal interface GithubApi {
 
@@ -11,4 +13,12 @@ internal interface GithubApi {
         @Path("owner") owner: String,
         @Path("name") name: String,
     ): List<ContributorResponse>
+
+    @GET("repos/{owner}/{name}/commits")
+    suspend fun getCommits(
+        @Path("owner") owner: String,
+        @Path("name") name: String,
+        @Query("author") author: String,
+        @Query("per_page") perPage: Int,
+    ): List<CommitResponse>
 }
