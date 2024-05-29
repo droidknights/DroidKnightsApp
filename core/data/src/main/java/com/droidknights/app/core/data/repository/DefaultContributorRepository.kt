3b6122dd6c105ddc82dfc6c4491c7b363080df41
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-05-29 12:01:41 +0900

    Revert "feat: filter to 2024 contributor"
    This reverts commit 25e137272cdd398883d184f5ba240407d5e7b9df.

--- core/data/src/main/java/com/droidknights/app/core/data/repository/DefaultContributorRepository.kt
+++ core/data/src/main/java/com/droidknights/app/core/data/repository/DefaultContributorRepository.kt
@@ -4,10 +4,6 @@ import com.droidknights.app.core.data.api.GithubApi
 import com.droidknights.app.core.data.mapper.toData
 import com.droidknights.app.core.data.repository.api.ContributorRepository
 import com.droidknights.app.core.model.Contributor
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 internal class DefaultContributorRepository @Inject constructor(
@@ -18,24 +14,7 @@ internal class DefaultContributorRepository @Inject constructor(
         owner: String,
         name: String,
     ): List<Contributor> {
-        val contributors = githubApi.getContributors(owner, name)
-        return withContext(Dispatchers.IO) {
-            contributors.map { contributor ->
-                async {
-                    val commits =
-                        githubApi.getCommits(owner, name, contributor.name, COUNT_FOR_LAST_COMMIT)
-                    if (commits.any { commit -> commit.commit.author.date.startsWith("2024") }) {
-                        contributor.toData()
-                    } else {
-                        null
-                    }
-                }
-            }.awaitAll().filterNotNull()
-        }
-    }
-
-    companion object {
-        private const val COUNT_FOR_LAST_COMMIT = 1
+        return githubApi.getContributors(owner, name)
+            .map { it.toData() }
     }
 }
-
--- core/data/src/test/java/com/droidknights/app/core/data/api/fake/FakeGithubApi.kt
+++ core/data/src/test/java/com/droidknights/app/core/data/api/fake/FakeGithubApi.kt
@@ -1,23 +1,10 @@
 package com.droidknights.app.core.data.api.fake
 
 import com.droidknights.app.core.data.api.GithubApi
-import com.droidknights.app.core.data.api.model.CommitResponse
 import com.droidknights.app.core.data.api.model.ContributorResponse
 
-internal class FakeGithubApi(
-    private val contributors: List<ContributorResponse>,
-    private val commits: List<CommitResponse>
-) : GithubApi {
+internal class FakeGithubApi(private val contributors: List<ContributorResponse>) : GithubApi {
     override suspend fun getContributors(owner: String, name: String): List<ContributorResponse> {
         return contributors
-    }
-
-    override suspend fun getCommits(
-        owner: String,
-        name: String,
-        author: String,
-        perPage: Int
-    ): List<CommitResponse> {
-        return commits
     }
 }
