511663fa5ee40fcb3fbced5520d316c382d346aa
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-05-30 12:11:28 +0900

    [feature/#306] Change kotlinOptions to compilerOptions

--- build-logic/src/main/kotlin/com/droidknights/app/KotlinAndroid.kt
+++ build-logic/src/main/kotlin/com/droidknights/app/KotlinAndroid.kt
@@ -5,8 +5,11 @@ package com.droidknights.app
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.provideDelegate
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinAndroidProjectExtension
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /**
@@ -51,15 +54,17 @@ internal fun Project.configureKotlinAndroid() {
 }
 
 internal fun Project.configureKotlin() {
-    tasks.withType<KotlinCompile>().configureEach {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_17.toString()
+    extensions.getByType<KotlinAndroidProjectExtension>().apply {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
             // Treat all Kotlin warnings as errors (disabled by default)
             // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
             val warningsAsErrors: String? by project
-            allWarningsAsErrors = warningsAsErrors.toBoolean()
-            freeCompilerArgs = freeCompilerArgs + listOf(
-                "-opt-in=kotlin.RequiresOptIn",
+            allWarningsAsErrors.set(warningsAsErrors.toBoolean())
+            freeCompilerArgs.set(
+                freeCompilerArgs.get() + listOf(
+                    "-opt-in=kotlin.RequiresOptIn",
+                )
             )
         }
     }
