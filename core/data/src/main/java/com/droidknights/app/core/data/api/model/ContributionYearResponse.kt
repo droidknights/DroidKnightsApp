05bcf722df1e8c69db38a364f133a4ef3b560314
kwakjoohyeong <rhkrwngud445@gmail.com>
2024-05-31 00:12:55 +0900

    feat: SerialName 적용

--- core/data/src/main/java/com/droidknights/app/core/data/api/model/ContributionYearResponse.kt
+++ core/data/src/main/java/com/droidknights/app/core/data/api/model/ContributionYearResponse.kt
@@ -1,9 +1,10 @@
 package com.droidknights.app.core.data.api.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ContributionYearResponse(
-    val id: Long,
-    val years: List<Int>,
+    @SerialName("id") val id: Long,
+    @SerialName("years") val years: List<Int>,
 )
