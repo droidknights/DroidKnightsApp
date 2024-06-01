4acf19f2c79620fc689fb5fcd53755d2d8af8503
HyunWoo Lee <l2hyunwoo@gmail.com>
2024-06-01 19:06:38 +0900

    Add hilt test manifest module

---
+++ core/ui-test-hilt-manifest/src/main/java/com/droidknights/app/core/uitesthiltmanifest/HiltComponentActivity.kt
@@ -0,0 +1,7 @@
+package com.droidknights.app.core.uitesthiltmanifest
+
+import androidx.activity.ComponentActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltComponentActivity : ComponentActivity()
