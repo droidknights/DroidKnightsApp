d74dd830e59687a84dbad77de4a7a1352462b6c1
JeonK1 <kan5016@naver.com>
2024-06-02 15:14:24 +0900

    delete : unused import

--- feature/session/src/main/java/com/droidknights/app/feature/session/SessionDetailScreen.kt
+++ feature/session/src/main/java/com/droidknights/app/feature/session/SessionDetailScreen.kt
@@ -30,7 +30,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
