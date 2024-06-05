cf912ada91fe9f36424b21e149080519dbe2bec2
buna <tmdgh1592@naver.com>
2024-06-05 13:24:43 +0900

    refactor: 모서리가 둥근 이미지를 RoundedImage 컴포넌트로 분리

---
+++ core/designsystem/src/main/java/com/droidknights/app/core/designsystem/component/RoundedImage.kt
@@ -0,0 +1,31 @@
+package com.droidknights.app.core.designsystem.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun RoundedImage(
+    modifier: Modifier = Modifier,
+    imageRes: Int,
+    onClick: () -> Unit = {},
+    roundSize: Dp = 16.dp,
+) {
+    Surface(
+        modifier = modifier,
+        shape = RoundedCornerShape(roundSize),
+        onClick = onClick,
+    ) {
+        Image(
+            painter = painterResource(id = imageRes),
+            contentDescription = null,
+            modifier = Modifier.aspectRatio(1f)
+        )
+    }
+}
