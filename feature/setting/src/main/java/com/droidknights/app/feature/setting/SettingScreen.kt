1e07d29bf4210aa50c51dae215785efd8e62ada9
buna <tmdgh1592@naver.com>
2024-06-05 21:37:54 +0900

    refactor: RoundedImage, IconToggleButton 제거

--- feature/setting/src/main/java/com/droidknights/app/feature/setting/SettingScreen.kt
+++ feature/setting/src/main/java/com/droidknights/app/feature/setting/SettingScreen.kt
@@ -2,30 +2,34 @@ package com.droidknights.app.feature.setting
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.droidknights.app.core.designsystem.component.KnightsCard
-import com.droidknights.app.core.designsystem.component.RoundedImage
 import com.droidknights.app.core.designsystem.theme.KnightsTheme
 import com.droidknights.app.core.designsystem.theme.LocalDarkTheme
 import com.droidknights.app.feature.setting.component.OpenSourceCard
@@ -105,10 +109,16 @@ private fun ThemeCard(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        RoundedImage(
-            imageRes = imageRes,
-            onClick = onClick
-        )
+        Surface(
+            shape = RoundedCornerShape(16.dp),
+            onClick = onClick,
+        ) {
+            Image(
+                painter = painterResource(id = imageRes),
+                contentDescription = null,
+                modifier = Modifier.aspectRatio(1f)
+            )
+        }
 
         Text(
             text = stringResource(id = titleRes),
