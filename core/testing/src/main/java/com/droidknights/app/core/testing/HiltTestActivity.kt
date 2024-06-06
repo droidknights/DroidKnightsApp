234d17afc37a46c89c485c1249f24ef0b22e66de
KwakEuiJin <yuijin0909@gmail.com>
2024-06-06 15:55:06 +0900

    HiltTestActivity.kt 삭제

--- core/testing/src/main/java/com/droidknights/app/core/testing/HiltTestActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.droidknights.app.core.testing
-
-import androidx.activity.ComponentActivity
-import dagger.hilt.android.AndroidEntryPoint
-
-@AndroidEntryPoint
-class HiltTestActivity : ComponentActivity()
