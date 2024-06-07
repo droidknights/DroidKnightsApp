c35c3563b829e339e97b9473b0c3a44a10542ef0
buna <tmdgh1592@naver.com>
2024-06-07 15:12:05 +0900

    refactor: sessionItems 확장 함수 제거

--- feature/session/src/main/java/com/droidknights/app/feature/session/SessionScreen.kt
+++ feature/session/src/main/java/com/droidknights/app/feature/session/SessionScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,19 +18,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.droidknights.app.core.designsystem.theme.KnightsTheme
-import com.droidknights.app.core.model.Room
 import com.droidknights.app.core.model.Session
 import com.droidknights.app.feature.session.component.SessionItem
 import com.droidknights.app.feature.session.component.SessionTopAppBar
 import com.droidknights.app.feature.session.model.SessionState
 import com.droidknights.app.feature.session.model.SessionUiState
 import com.droidknights.app.feature.session.model.rememberSessionState
-import kotlinx.collections.immutable.PersistentList
 import kotlinx.collections.immutable.persistentListOf
 import kotlinx.coroutines.flow.collectLatest
 
@@ -81,46 +77,33 @@ private fun SessionList(
         contentPadding = PaddingValues(horizontal = 8.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
-        sessionState.groups.forEachIndexed { index, group ->
-            val topPadding = if (index == 0) {
-                SessionTopSpace
-            } else {
-                SessionGroupSpace
+        sessionState.groups.forEachIndexed { groupIndex, group ->
+            itemsIndexed(group.sessions) { sessionIndex, session ->
+                SessionItem(
+                    index = sessionIndex,
+                    item = session,
+                    room = group.room,
+                    topPadding = if (groupIndex == 0) {
+                        SessionTopSpace
+                    } else {
+                        SessionGroupSpace
+                    },
+                    onItemClick = onSessionClick
+                )
+
+                val isLastGroup = groupIndex == sessionState.groups.size - 1
+                val isLastSession = sessionIndex == group.sessions.size - 1
+
+                if (isLastGroup && isLastSession) {
+                    DroidKnightsFooter()
+                }
             }
-            sessionItems(
-                room = group.room,
-                items = group.sessions,
-                topPadding = topPadding,
-                isLastGroup = index == sessionState.groups.size - 1,
-                onItemClick = onSessionClick,
-            )
         }
     }
 }
 
 private val SessionTopSpace = 16.dp
 private val SessionGroupSpace = 100.dp
-
-private fun LazyListScope.sessionItems(
-    room: Room,
-    items: PersistentList<Session>,
-    topPadding: Dp,
-    isLastGroup: Boolean,
-    onItemClick: (Session) -> Unit,
-) {
-    itemsIndexed(items) { index, item ->
-        SessionItem(
-            index = index,
-            item = item,
-            room = room,
-            topPadding = topPadding,
-            onItemClick = onItemClick
-        )
-        if (isLastGroup && index == items.size - 1) {
-            DroidKnightsFooter()
-        }
-    }
-}
 
 @Composable
 private fun DroidKnightsFooter() {
