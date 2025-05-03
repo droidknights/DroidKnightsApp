5f28bf6bba2470dd911ef7d1fd6256a4c4d25e66
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-05-03 12:49:53 +0900

    [feature/shared-model] Shared 모듈 gradle 정리

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,27 +1,14 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    id("droidknights.kotlin.multiplatform")
 }
 
 kotlin {
-    androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
-        compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
-        }
-    }
-    
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
-    
     jvm()
-    
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
         browser {
@@ -46,14 +33,4 @@ kotlin {
     }
 }
 
-android {
-    namespace = "com.droidknights.app.shared"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    defaultConfig {
-        minSdk = libs.versions.android.minSdk.get().toInt()
-    }
-}
+android.namespace = "com.droidknights.app.shared"
