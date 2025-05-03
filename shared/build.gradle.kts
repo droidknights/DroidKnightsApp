a022dfd576a56459b2f65bf3d6621d9a2df89965
HyunWoo Lee (Nunu Lee) <54518925+l2hyunwoo@users.noreply.github.com>
2025-05-03 14:51:03 +0900

    Update shared/build.gradle.kts

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -30,7 +30,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.serialization.json)
-            implementation(libs.kotlinx.datetime)
+            api(libs.kotlinx.datetime)
         }
     }
 }
