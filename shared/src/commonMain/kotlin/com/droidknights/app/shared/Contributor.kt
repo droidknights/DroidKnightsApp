c3ed1e9ed20caa0a3bc296090a66087e7713f8e5
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-05-03 13:42:30 +0900

    [feature/shared-model] Serializable 부착

--- shared/src/commonMain/kotlin/com/droidknights/app/shared/Contributor.kt
+++ shared/src/commonMain/kotlin/com/droidknights/app/shared/Contributor.kt
@@ -1,8 +1,16 @@
 package com.droidknights.app.shared
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Contributor(
+    @SerialName("id")
     val id: Long,
+    @SerialName("name")
     val name: String,
+    @SerialName("imageUrl")
     val imageUrl: String,
+    @SerialName("githubUrl")
     val githubUrl: String,
 )
--- shared/src/commonMain/kotlin/com/droidknights/app/shared/Room.kt
+++ shared/src/commonMain/kotlin/com/droidknights/app/shared/Room.kt
@@ -1,5 +1,8 @@
 package com.droidknights.app.shared
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 enum class Room {
     ETC,
     TRACK1,
