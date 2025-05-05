977c4358f3ea83e99a6f5744462fdb3a18144162
workspace <ryukimin.workspace@gmail.com>
2025-05-05 11:58:18 +0900

    [init-modules] 임시 디자인 컴포넌트에 누락된 modifier 추가

--- core/designsystem/src/commonMain/kotlin/com/droidknights/app/core/designsystem/components/Button.kt
+++ core/designsystem/src/commonMain/kotlin/com/droidknights/app/core/designsystem/components/Button.kt
@@ -1,14 +1,17 @@
 package com.droidknights.app.core.designsystem.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun Button(
     text: String,
-    onClick: () -> Unit
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
 ) {
     androidx.compose.material3.Button(
-        onClick = onClick
+        onClick = onClick,
+        modifier = modifier
     ) {
         androidx.compose.material3.Text(text)
     }
