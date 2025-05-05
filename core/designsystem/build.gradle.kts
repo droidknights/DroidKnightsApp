5dccef28f684558fdafaf05f98aedc9e7946da7b
workspace <ryukimin.workspace@gmail.com>
2025-05-05 11:40:13 +0900

    [init-modules] jvm, wasm 플러그인 반영

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,6 +1,3 @@
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
-
 plugins {
     alias(libs.plugins.androidLibrary)
     id("droidknights.kotlin.multiplatform")
@@ -8,25 +5,6 @@ plugins {
 }
 
 kotlin {
-    jvm()
-
-    @OptIn(ExperimentalWasmDsl::class)
-    wasmJs {
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
-    }
-
     sourceSets {
         commonMain.dependencies {
             implementation(compose.material3)
