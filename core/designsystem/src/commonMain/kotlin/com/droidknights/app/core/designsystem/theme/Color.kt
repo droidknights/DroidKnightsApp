b5a3e472a9b26654c90a8f5db59fd85597893ccf
workspace <ryukimin.workspace@gmail.com>
2025-05-06 14:03:40 +0900

    [init-theme] 기본 색상 및 m3 테마 정의

--- core/designsystem/src/commonMain/kotlin/com/droidknights/app/core/designsystem/theme/Color.kt
+++ core/designsystem/src/commonMain/kotlin/com/droidknights/app/core/designsystem/theme/Color.kt
@@ -2,5 +2,12 @@ package com.droidknights.app.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-// TODO Figma 보고 현행화 해야함
-val Blue01 = Color(0xFF215BF6)
+val Blue01 = Color(0xFF5180FF)
+val Blue02 = Color(0xFF215BF6)
+
+val White = Color(0xFFFFFFFF)
+val PaleGray = Color(0xFFF9F9F9)
+val LightGray = Color(0xFFDCDCDC)
+val DarkGray = Color(0xFF5E5E5E)
+val Black = Color(0xFF000000)
+val Graphite = Color(0xFF292929)
