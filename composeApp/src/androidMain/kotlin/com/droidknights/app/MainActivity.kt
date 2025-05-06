47a1d5663b50b68a1e0b7acfa6588803d0dc504a
workspace <ryukimin.workspace@gmail.com>
2025-05-06 16:29:58 +0900

    [init-theme][Android] 안드로이드 시스템 아이콘 색상 다크모드 대응

--- composeApp/src/androidMain/kotlin/com/droidknights/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/droidknights/app/MainActivity.kt
@@ -1,16 +1,38 @@
 package com.droidknights.app
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.auto(
+                lightScrim = Color.TRANSPARENT,
+                darkScrim = Color.TRANSPARENT
+            )
+        )
 
         setContent {
+            val view = LocalView.current
+            val darkTheme = isSystemInDarkTheme()
+            SideEffect {
+                with(WindowCompat.getInsetsController(window, view)) {
+                    isAppearanceLightStatusBars = !darkTheme
+                    isAppearanceLightNavigationBars = !darkTheme
+                }
+            }
+
             App()
         }
     }
