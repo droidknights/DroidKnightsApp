ffd86f14e7b4cb6e1e0de7837f82f6c14ce0bcbc
workspace <ryukimin.workspace@gmail.com>
2025-05-08 01:12:25 +0900

    [remove-material3] yarn.lock 문제를 해결시도를 위해 임시로 material3 복구

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -4,4 +4,13 @@ plugins {
     id("droidknights.compose.multiplatform")
 }
 
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
+        }
+    }
+}
+
 android.namespace = "com.droidknights.app.core.designsystem"
