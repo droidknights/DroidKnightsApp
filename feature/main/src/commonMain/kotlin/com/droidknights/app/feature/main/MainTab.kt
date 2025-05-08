1459eec3e5b1e041c5d9e91ec681f553aab3b4f3
workspace <ryukimin.workspace@gmail.com>
2025-05-08 21:16:55 +0900

    [main-bottom-bar] MainBottomBar 구현

---
+++ feature/main/src/commonMain/kotlin/com/droidknights/app/feature/main/MainTab.kt
@@ -0,0 +1,44 @@
+package com.droidknights.app.feature.main
+
+import androidx.compose.runtime.Composable
+import com.droidknights.app.core.navigation.MainTabRoute
+import com.droidknights.app.core.navigation.Route
+import droidknights.feature.main.generated.resources.Res
+import droidknights.feature.main.generated.resources.ic_bookmark
+import droidknights.feature.main.generated.resources.ic_home
+import droidknights.feature.main.generated.resources.ic_setting
+import org.jetbrains.compose.resources.DrawableResource
+
+internal enum class MainTab(
+    val iconResId: DrawableResource,
+    internal val contentDescription: String,
+    val route: MainTabRoute,
+) {
+    SETTING(
+        iconResId = Res.drawable.ic_setting,
+        contentDescription = "설정",
+        MainTabRoute.Setting,
+    ),
+    HOME(
+        iconResId = Res.drawable.ic_home,
+        contentDescription = "홈",
+        MainTabRoute.Home
+    ),
+    BOOKMARK(
+        iconResId = Res.drawable.ic_bookmark,
+        contentDescription = "북마크",
+        MainTabRoute.Bookmark,
+    );
+
+    companion object {
+        @Composable
+        fun find(predicate: @Composable (MainTabRoute) -> Boolean): MainTab? {
+            return entries.find { predicate(it.route) }
+        }
+
+        @Composable
+        fun contains(predicate: @Composable (Route) -> Boolean): Boolean {
+            return entries.map { it.route }.any { predicate(it) }
+        }
+    }
+}
---
+++ feature/main/src/commonMain/kotlin/com/droidknights/app/feature/main/components/MainBottomBar.kt
@@ -0,0 +1,136 @@
+package com.droidknights.app.feature.main.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideIn
+import androidx.compose.animation.slideOut
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import com.droidknights.app.core.designsystem.components.Button
+import com.droidknights.app.core.designsystem.components.Icon
+import com.droidknights.app.core.designsystem.components.Surface
+import com.droidknights.app.core.designsystem.theme.KnightsTheme
+import com.droidknights.app.feature.main.MainTab
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+internal fun BoxScope.MainBottomBar(
+    visible: Boolean,
+    currentTab: MainTab?,
+    onTabSelected: (MainTab) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    AnimatedVisibility(
+        modifier = Modifier
+            .align(Alignment.BottomCenter)
+            .navigationBarsPadding(),
+        visible = visible,
+        enter = fadeIn() + slideIn { IntOffset(0, it.height) },
+        exit = fadeOut() + slideOut { IntOffset(0, it.height) }
+    ) {
+        Surface(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            shape = CircleShape,
+            border = BorderStroke(1.dp, KnightsTheme.colorScheme.borderColor)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 28.dp)
+                    .selectableGroup(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                MainTab.entries.forEach { tab ->
+                    MainBottomBarItem(
+                        tab = tab,
+                        selected = tab == currentTab,
+                        onClick = { onTabSelected(tab) },
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun RowScope.MainBottomBarItem(
+    modifier: Modifier = Modifier,
+    tab: MainTab,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    Box(
+        modifier = modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .selectable(
+                selected = selected,
+                role = Role.Tab,
+                onClick = onClick,
+            ),
+        contentAlignment = Alignment.Center,
+    ) {
+        Icon(
+            painter = painterResource(tab.iconResId),
+            contentDescription = tab.contentDescription,
+            tint = if (selected) {
+                KnightsTheme.colorScheme.selectedIconColor
+            } else {
+                KnightsTheme.colorScheme.unselectedIconColor
+            },
+            modifier = Modifier.size(32.dp),
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun MainBottomBarPreview() {
+    KnightsTheme {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            var show by remember { mutableStateOf(true) }
+
+            Button(
+                text = "show/hide",
+                onClick = { show = !show },
+                modifier = Modifier.align(Alignment.Center)
+            )
+            MainBottomBar(
+                visible = show,
+                currentTab = MainTab.HOME,
+                onTabSelected = { },
+            )
+        }
+    }
+}
