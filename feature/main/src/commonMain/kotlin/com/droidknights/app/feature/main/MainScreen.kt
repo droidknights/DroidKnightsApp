cf0e4f67b395cea2768d01949d297e49454d2a25
workspace <ryukimin.workspace@gmail.com>
2025-05-08 21:17:47 +0900

    [main-bottom-bar] MainScreen에 MainBottomBar 추가

--- feature/main/src/commonMain/kotlin/com/droidknights/app/feature/main/MainScreen.kt
+++ feature/main/src/commonMain/kotlin/com/droidknights/app/feature/main/MainScreen.kt
@@ -1,10 +1,12 @@
 package com.droidknights.app.feature.main
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.droidknights.app.core.designsystem.components.Surface
 import com.droidknights.app.core.designsystem.theme.KnightsTheme
+import com.droidknights.app.feature.main.components.MainBottomBar
 import com.droidknights.app.feature.main.components.MainNavHost
 
 @Composable
@@ -15,6 +17,14 @@ fun MainScreen(
         modifier = modifier.fillMaxSize(),
         color = KnightsTheme.colorScheme.background
     ) {
-        MainNavHost()
+        Box(modifier = Modifier.fillMaxSize()) {
+            MainNavHost()
+
+            MainBottomBar(
+                visible = true,
+                currentTab = MainTab.HOME,
+                onTabSelected = { },
+            )
+        }
     }
 }
