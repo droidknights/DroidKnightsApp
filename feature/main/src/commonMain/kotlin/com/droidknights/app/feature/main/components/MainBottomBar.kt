0fb6b8de4059f86ef943fbccc7a21b690be185d8
workspace <ryukimin.workspace@gmail.com>
2025-05-09 02:58:35 +0900

    [desktop-web] MainBottomBar 하단 마진 추가

--- feature/main/src/commonMain/kotlin/com/droidknights/app/feature/main/components/MainBottomBar.kt
+++ feature/main/src/commonMain/kotlin/com/droidknights/app/feature/main/components/MainBottomBar.kt
@@ -56,7 +56,7 @@ internal fun BoxScope.MainBottomBar(
     ) {
         Surface(
             modifier = modifier
-                .padding(horizontal = 8.dp)
+                .padding(8.dp)
                 .fillMaxWidth()
                 .height(56.dp),
             shape = CircleShape,
