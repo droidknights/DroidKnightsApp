d3bc16de7d3eef014c1ef07b2d8b2619557a9797
workspace <ryukimin.workspace@gmail.com>
2025-05-11 01:08:33 +0900

    [setting] 오픈 소스 카드 추가

--- feature/setting/src/commonMain/kotlin/com/droidknights/app/feature/setting/SettingScreen.kt
+++ feature/setting/src/commonMain/kotlin/com/droidknights/app/feature/setting/SettingScreen.kt
@@ -3,21 +3,33 @@ package com.droidknights.app.feature.setting
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.droidknights.app.core.designsystem.components.Text
+import com.droidknights.app.feature.setting.components.SettingOpenSourceCard
 
 @Composable
 internal fun SettingScreen(
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .systemBarsPadding()
+            .verticalScroll(rememberScrollState())
+            .fillMaxSize()
+            .padding(8.dp)
+            .padding(bottom = 64.dp), // MainBottomBar 56(높이) + 8(하단 패딩)
+        verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        Text("SettingScreen")
+        SettingOpenSourceCard(
+            onClick = {
+            },
+            modifier = Modifier.fillMaxWidth(),
+        )
     }
 }
