958e1a029b604a695e437f02aa7dc013b98c38bb
workspace <ryukimin.workspace@gmail.com>
2025-05-10 18:59:44 +0900

    [home] HomeContributorCard 추가

--- feature/home/src/commonMain/kotlin/com/droidknights/app/feature/home/HomeScreen.kt
+++ feature/home/src/commonMain/kotlin/com/droidknights/app/feature/home/HomeScreen.kt
@@ -6,14 +6,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.droidknights.app.core.designsystem.components.Button
+import com.droidknights.app.core.designsystem.theme.KnightsTheme
+import com.droidknights.app.feature.home.components.HomeContributorCard
 import com.droidknights.app.feature.home.components.HomeSessionCard
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 internal fun HomeScreen(
@@ -31,7 +33,11 @@ internal fun HomeScreen(
     ) {
         HomeSessionCard(
             onClick = onSessionClick,
-            modifier = Modifier.fillMaxWidth().wrapContentHeight(),
+            modifier = Modifier.fillMaxWidth(),
+        )
+        HomeContributorCard(
+            onClick = onContributorClick,
+            modifier = Modifier.fillMaxWidth(),
         )
         Button(
             text = "Contributor",
