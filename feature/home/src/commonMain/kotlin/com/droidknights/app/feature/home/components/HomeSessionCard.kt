28f4bb01b6f5a63e1d251f1053c897771edf0b61
workspace <ryukimin.workspace@gmail.com>
2025-05-21 01:03:54 +0900

    [home-cards] 홈 세션 카드 배경을 png로 변경
    android 외 shader가 의도대로 동작하지 않는 문제가 있었음

--- feature/home/src/commonMain/kotlin/com/droidknights/app/feature/home/components/HomeSessionCard.kt
+++ feature/home/src/commonMain/kotlin/com/droidknights/app/feature/home/components/HomeSessionCard.kt
@@ -1,23 +1,25 @@
 package com.droidknights.app.feature.home.components
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.droidknights.app.core.designsystem.components.Surface
 import com.droidknights.app.core.designsystem.components.Text
 import com.droidknights.app.core.designsystem.theme.KnightsTheme
 import com.droidknights.app.core.designsystem.theme.LocalContentColor
-import com.droidknights.app.core.shader.components.BottomUpGlowBackground
 import droidknights.feature.home.generated.resources.Res
+import droidknights.feature.home.generated.resources.background_home_session_card
 import droidknights.feature.home.generated.resources.home_session_card_desc
 import droidknights.feature.home.generated.resources.home_session_card_title
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -32,10 +34,15 @@ fun HomeSessionCard(
         color = KnightsTheme.colorScheme.primary,
         shape = RoundedCornerShape(16.dp),
     ) {
-        BottomUpGlowBackground {
+        Box {
+            Image(
+                modifier = Modifier.matchParentSize(),
+                painter = painterResource(Res.drawable.background_home_session_card),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+            )
             Column(
                 modifier = Modifier
-                    .background(Color.Black.copy(alpha = 0.16F))
                     .fillMaxWidth()
                     .padding(24.dp, 18.dp, 24.dp, 38.dp),
                 verticalArrangement = Arrangement.spacedBy(8.dp),
