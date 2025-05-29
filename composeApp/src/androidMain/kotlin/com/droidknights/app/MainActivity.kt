569e50808fb9f965f84b9f6ec21e5b05c76f76f1
yeojun-yoon <yjyoon.dev@navercorp.com>
2025-05-29 23:40:03 +0900

    [#412] android 타겟에서 koin 초기화를 Application 에서 수행

--- composeApp/src/androidMain/kotlin/com/droidknights/app/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/droidknights/app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.droidknights.app
 
-import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,8 +8,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
-import org.koin.compose.KoinApplication
-import org.koin.dsl.module
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,24 +22,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             val view = LocalView.current
-            val platformModule = module {
-                single<Context> { this@MainActivity }
-            }
 
-            KoinApplication(
-                application = koinAppDeclaration {
-                    modules(platformModule)
+            App(
+                onDarkThemeChange = { darkTheme ->
+                    with(WindowCompat.getInsetsController(window, view)) {
+                        isAppearanceLightStatusBars = !darkTheme
+                        isAppearanceLightNavigationBars = !darkTheme
+                    }
                 },
-            ) {
-                App(
-                    onDarkThemeChange = { darkTheme ->
-                        with(WindowCompat.getInsetsController(window, view)) {
-                            isAppearanceLightStatusBars = !darkTheme
-                            isAppearanceLightNavigationBars = !darkTheme
-                        }
-                    },
-                )
-            }
+            )
         }
     }
 }
