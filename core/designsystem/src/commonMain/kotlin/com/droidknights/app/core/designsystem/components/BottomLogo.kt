2e203703f256f681b1dd7f8bfcbd51f35324cdef
JunJangE <wnswkd486@gmail.com>
2025-05-30 22:06:45 +0900

    [droidknights#411] BottomLogo 구현

---
+++ core/designsystem/src/commonMain/kotlin/com/droidknights/app/core/designsystem/components/BottomLogo.kt
@@ -0,0 +1,31 @@
+package com.droidknights.app.core.designsystem.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.droidknights.app.core.designsystem.theme.KnightsTheme
+import com.droidknights.app.core.designsystem.theme.LightGray
+
+@Composable
+fun BottomLogo(
+    modifier: Modifier = Modifier,
+    color: Color = LightGray,
+) {
+    Box(
+        modifier = modifier.fillMaxWidth().height(BottomLogoHeight),
+        contentAlignment = Alignment.Center,
+    ) {
+        Text(
+            text = "Droid Knights 2023",
+            style = KnightsTheme.typography.labelMediumR,
+            color = color,
+        )
+    }
+}
+
+internal val BottomLogoHeight = 48.dp
