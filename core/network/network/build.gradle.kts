6199b477d27dc9ce8409353c41eee473a3404c04
LEE YOU BIN <youbeene3409@naver.com>
2025-06-01 15:22:44 +0900

    [feature/#409] Ktor 라이브러리 추가

--- core/network/network/build.gradle.kts
+++ core/network/network/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     id("droidknights.kotlin.multiplatform")
 }
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.ktor.client.content.negotiation )
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.serialization.kotlinx.json)
+        }
+    }
+}
