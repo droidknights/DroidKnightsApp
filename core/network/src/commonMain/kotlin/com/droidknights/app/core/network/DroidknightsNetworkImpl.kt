83a26f70fb105f24d9af62b22342cad3d68783b8
LEE YOU BIN <youbeene3409@naver.com>
2025-06-01 22:31:05 +0900

    [feature/#409] NetworkModule Ktor 구현

--- core/network/src/commonMain/kotlin/com/droidknights/app/core/network/DroidknightsNetworkImpl.kt
+++
@@ -1,4 +0,0 @@
-package com.droidknights.app.core.network
-
-class DroidknightsNetworkImpl {
-}
--- core/network/src/commonMain/kotlin/com/droidknights/app/core/network/di/NetworkModule.kt
+++ core/network/src/commonMain/kotlin/com/droidknights/app/core/network/di/NetworkModule.kt
@@ -1,4 +1,48 @@
 package com.droidknights.app.core.network.di
 
-class NetworkModule {
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.KotlinxSerializationConverter
+import kotlinx.serialization.json.Json
+import org.koin.dsl.module
+
+val networkModule = module {
+
+    single {
+
+        val json = Json {
+            ignoreUnknownKeys = true
+            encodeDefaults = true
+        }
+
+        HttpClient {
+            install(ContentNegotiation) {
+                register(ContentType.Application.Json, KotlinxSerializationConverter(json))
+                register(ContentType.Text.Plain, KotlinxSerializationConverter(json))
+            }
+            install(Logging) {
+                level = LogLevel.ALL
+            }
+            install(HttpTimeout) {
+                connectTimeoutMillis = 6000
+                requestTimeoutMillis = 6000
+                socketTimeoutMillis = 6000
+            }
+            defaultRequest {
+                contentType(ContentType.Application.Json)
+                url {
+                    protocol = URLProtocol.HTTPS
+                    host = "raw.githubusercontent.com"
+                }
+            }
+        }
+    }
+
 }
