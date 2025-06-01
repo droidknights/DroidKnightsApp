75a90008f93057a0c8ac327e86e7277351a5a3d7
LEE YOU BIN <youbeene3409@naver.com>
2025-06-01 22:30:36 +0900

    [feature/#409] :core:network 네임스페이스 추가

--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -6,13 +6,14 @@ plugins {
 kotlin {
     sourceSets {
         commonMain.dependencies {
-            // ktor
             implementation(libs.ktor.client.content.negotiation )
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.ktor.logging)
-            // koin
+
             implementation(libs.koin.core)
         }
     }
 }
+
+android.namespace = "com.droidknights.app.core.network"
