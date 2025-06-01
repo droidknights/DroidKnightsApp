361371ae4467f75a72c2fc5e9ae6f5602980d13c
LEE YOU BIN <youbeene3409@naver.com>
2025-06-01 14:20:07 +0900

    [feature/#409] KMP 플러그인 추가

--- core/network/network/build.gradle.kts
+++ core/network/network/build.gradle.kts
@@ -1,13 +1,3 @@
 plugins {
-    id("java-library")
-    alias(libs.plugins.kotlinJvm)
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
-    }
+    id("droidknights.kotlin.multiplatform")
 }
