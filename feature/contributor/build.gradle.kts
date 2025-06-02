d1855c4273f351a775d9e6879df2f8c9932c9e10
JunJangE <wnswkd486@gmail.com>
2025-06-02 14:24:34 +0900

    [droidknights#411] immutable을 제외한 나머지 의존성 제거

--- feature/contributor/build.gradle.kts
+++ feature/contributor/build.gradle.kts
@@ -6,11 +6,6 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(libs.kotlinx.immutable)
-            implementation(libs.koin.compose.viewmodel.navigation)
-            implementation(libs.androidx.navigation.compose)
-            implementation(libs.androidx.lifecycle.runtime.compose)
-            implementation(projects.core.designsystem)
-            implementation(projects.core.navigation)
         }
     }
 }
