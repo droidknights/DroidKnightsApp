71a64c321c171f70483cc9813b3cb1ff1c469d55
LEE YOU BIN <youbeene3409@naver.com>
2025-06-02 17:31:17 +0900

    [feature/#409] SessionApi에서 DroidknightsNetwork 이용하도록 구현

--- core/data/data-session/src/commonMain/kotlin/com/droidknights/app/core/data/session/SessionApi.kt
+++ core/data/data-session/src/commonMain/kotlin/com/droidknights/app/core/data/session/SessionApi.kt
@@ -1,12 +1,12 @@
 package com.droidknights.app.core.data.session
 
 import com.droidknights.app.core.data.session.model.SessionResponse
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
+import com.droidknights.app.core.network.DroidknightsNetwork
+import com.droidknights.app.core.network.get
 
-class SessionApi(private val client: HttpClient) {
-    suspend fun getSessions(): List<SessionResponse> = client
-        .get("/droidknights/DroidKnightsApp/main/core/data/src/main/assets/sessions.json")
-        .body()
+class SessionApi(
+    private val network: DroidknightsNetwork
+) {
+    suspend fun getSessions(): List<SessionResponse> =
+        network.get("/droidknights/DroidKnightsApp/main/core/data/src/main/assets/sessions.json")
 }
