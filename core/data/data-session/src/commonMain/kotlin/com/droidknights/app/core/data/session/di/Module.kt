3939f572de409a495178087aa52bd5428765d595
LEE YOU BIN <youbeene3409@naver.com>
2025-06-02 16:29:35 +0900

    [feature/#409] 컨플릭 해결

--- core/data/data-session/src/commonMain/kotlin/com/droidknights/app/core/data/session/di/Module.kt
+++ core/data/data-session/src/commonMain/kotlin/com/droidknights/app/core/data/session/di/Module.kt
@@ -7,5 +7,5 @@ import org.koin.dsl.module
 
 val coreDataSessionModule = module {
     single { SessionApi(get()) }
-    single<SessionRepository> { SessionRepositoryImpl(get()) }
+    single<SessionRepository> { SessionRepositoryImpl(get(), get()) }
 }
