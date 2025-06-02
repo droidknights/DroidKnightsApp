e5ca89268d97f7f2b89ae6129a69a0bbf015c51c
MinJae Han <hanminjae131@gmail.com>
2025-06-03 02:16:28 +0900

    [feature] SessionDetailViewModel 구현

--- feature/session/src/commonMain/kotlin/com/droidknights/app/feature/session/SessionDetailViewModel.kt
+++ feature/session/src/commonMain/kotlin/com/droidknights/app/feature/session/SessionDetailViewModel.kt
@@ -2,26 +2,71 @@ package com.droidknights.app.feature.session
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.droidknights.app.core.domain.session.api.usecase.BookmarkSessionUseCase
+import com.droidknights.app.core.domain.session.api.usecase.GetBookmarkedSessionIdsUseCase
 import com.droidknights.app.core.domain.session.api.usecase.GetSessionUseCase
+import com.droidknights.app.feature.session.model.SessionDetailEffect
 import com.droidknights.app.feature.session.model.SessionDetailUiState
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 internal class SessionDetailViewModel(
     private val getSessionUseCase: GetSessionUseCase,
+    getBookmarkedSessionIdsUseCase: GetBookmarkedSessionIdsUseCase,
+    private val bookmarkSessionUseCase: BookmarkSessionUseCase,
 ) : ViewModel() {
 
-    private val _uiState =
-        MutableStateFlow<SessionDetailUiState>(SessionDetailUiState.Loading)
+    private val _uiState = MutableStateFlow<SessionDetailUiState>(SessionDetailUiState.Loading)
     val uiState = _uiState.asStateFlow()
+
+    private val _sessionUiEffect = MutableStateFlow<SessionDetailEffect>(SessionDetailEffect.Idle)
+    val sessionUiEffect = _sessionUiEffect.asStateFlow()
+
+    init {
+        combine(
+            uiState,
+            getBookmarkedSessionIdsUseCase(),
+        ) { uiState, bookmarkIds ->
+            when (uiState) {
+                is SessionDetailUiState.Loading -> uiState
+                is SessionDetailUiState.Success -> {
+                    uiState.copy(bookmarked = bookmarkIds.contains(uiState.session.id))
+                }
+            }
+        }
+            .onEach { _uiState.value = it }
+            .launchIn(viewModelScope)
+    }
 
     fun fetchSession(sessionId: String) {
         viewModelScope.launch {
+            _uiState.value = SessionDetailUiState.Loading
+            delay(1000L)
             val session = getSessionUseCase(sessionId)
             _uiState.value = SessionDetailUiState.Success(session)
         }
     }
 
-    // TODO 북마크
+    fun toggleBookmark() {
+        val uiState = uiState.value
+        if (uiState !is SessionDetailUiState.Success) {
+            return
+        }
+        viewModelScope.launch {
+            val bookmark = uiState.bookmarked
+            bookmarkSessionUseCase(uiState.session.id, !bookmark)
+            _sessionUiEffect.value = SessionDetailEffect.ShowToastForBookmarkState(!bookmark)
+        }
+    }
+
+    fun hidePopup() {
+        viewModelScope.launch {
+            _sessionUiEffect.value = SessionDetailEffect.Idle
+        }
+    }
 }
