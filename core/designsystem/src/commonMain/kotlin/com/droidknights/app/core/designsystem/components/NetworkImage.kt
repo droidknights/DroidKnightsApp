474bedc738f521ec95e72aa3c065397a23e55bc3
mijeongko <mijeong.ko@hunet.co.kr>
2025-06-04 11:29:40 +0900

    [#466][PR반영]LocalAsyncImagePreviewHandler를 이용해 프리뷰 대응 - 디폴트 이미지 제거

--- core/designsystem/src/commonMain/kotlin/com/droidknights/app/core/designsystem/components/NetworkImage.kt
+++ core/designsystem/src/commonMain/kotlin/com/droidknights/app/core/designsystem/components/NetworkImage.kt
@@ -1,17 +1,22 @@
 package com.droidknights.app.core.designsystem.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.layout.ContentScale
+import coil3.ColorImage
+import coil3.annotation.ExperimentalCoilApi
 import coil3.compose.AsyncImage
+import coil3.compose.AsyncImagePreviewHandler
+import coil3.compose.LocalAsyncImagePreviewHandler
 import coil3.compose.LocalPlatformContext
 import coil3.request.ImageRequest
 import coil3.request.crossfade
-import droidknights.core.designsystem.generated.resources.Res
-import droidknights.core.designsystem.generated.resources.playstore_m
-import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
 fun NetworkImage(
     imageUrl: String?,
@@ -20,17 +25,20 @@ fun NetworkImage(
     contentScale: ContentScale = ContentScale.Crop,
     contentDescription: String? = null,
 ) {
-    val painter = placeholder ?: painterResource(Res.drawable.playstore_m)
+    val previewHandler = AsyncImagePreviewHandler {
+        ColorImage(Color.Red.toArgb())
+    }
 
-    AsyncImage(
-        model = ImageRequest.Builder(LocalPlatformContext.current)
-            .data(imageUrl)
-            .crossfade(true)
-            .build(),
-        modifier = modifier,
-        placeholder = placeholder,
-        contentScale = contentScale,
-        contentDescription = contentDescription,
-        error = painter,
-    )
+    CompositionLocalProvider(LocalAsyncImagePreviewHandler provides previewHandler) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalPlatformContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .build(),
+            modifier = modifier,
+            placeholder = placeholder,
+            contentScale = contentScale,
+            contentDescription = contentDescription,
+        )
+    }
 }
