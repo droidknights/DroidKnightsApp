fb95ac223bb793cc6480746663f4c89760339eed
s9hn <tnghqn@naver.com>
2025-06-05 17:48:22 +0900

    (REFACTOR)[droidknights#422] 사용하지 않는 Navigate 함수 제거

--- feature/home/src/main/java/com/droidknights/app/feature/home/navigation/HomeNavigation.kt
+++ feature/home/src/main/java/com/droidknights/app/feature/home/navigation/HomeNavigation.kt
@@ -1,16 +1,10 @@
 package com.droidknights.app.feature.home.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.droidknights.app.core.navigation.MainTabRoute
 import com.droidknights.app.feature.home.HomeRoute
-
-fun NavController.navigateHome(navOptions: NavOptions) {
-    navigate(MainTabRoute.Home, navOptions)
-}
 
 fun NavGraphBuilder.homeNavGraph(
     padding: PaddingValues,
--- feature/session/src/main/java/com/droidknights/app/feature/session/navigation/SessionNavigation.kt
+++ feature/session/src/main/java/com/droidknights/app/feature/session/navigation/SessionNavigation.kt
@@ -1,6 +1,5 @@
 package com.droidknights.app.feature.session.navigation
 
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
@@ -8,10 +7,6 @@ import com.droidknights.app.feature.session.api.RouteSession
 import com.droidknights.app.feature.session.api.RouteSessionDetail
 import com.droidknights.app.feature.session.detail.SessionDetailScreen
 import com.droidknights.app.feature.session.list.SessionScreen
-
-fun NavController.navigateSessionDetail(sessionId: String) {
-    navigate(RouteSessionDetail(sessionId))
-}
 
 fun NavGraphBuilder.sessionNavGraph(
     onShowErrorSnackBar: (throwable: Throwable?) -> Unit,
