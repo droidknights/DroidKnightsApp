3846483fc9bbf88973877058bc0e2dc0bab4624d
workspace <ryukimin.workspace@gmail.com>
2025-06-06 22:37:13 +0900

    [#493] fix detekt

--- feature/home/src/main/java/com/droidknights/app/feature/home/component/SponsorCard.kt
+++ feature/home/src/main/java/com/droidknights/app/feature/home/component/SponsorCard.kt
@@ -4,11 +4,9 @@ import android.R.attr.onClick
 import android.content.res.Configuration
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.MarqueeSpacing
 import androidx.compose.foundation.background
 import androidx.compose.foundation.basicMarquee
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -19,7 +17,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,7 +24,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -227,7 +223,6 @@ private fun IndividualSponsors(
         }
     }
 }
-
 
 private sealed class Sponsor {
     data class Organization(
