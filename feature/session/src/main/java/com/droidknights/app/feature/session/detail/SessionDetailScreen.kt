22dd11e5a1bcd45d4bb255c9916bd3bb96aa90c6
s9hn <tnghqn@naver.com>
2025-06-08 16:39:01 +0900

    (REFACTOR)[droidknights#422] CI 에러 해결

--- feature/session/src/main/java/com/droidknights/app/feature/session/detail/SessionDetailScreen.kt
+++ feature/session/src/main/java/com/droidknights/app/feature/session/detail/SessionDetailScreen.kt
@@ -1,6 +1,5 @@
 package com.droidknights.app.feature.session.detail
 
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
