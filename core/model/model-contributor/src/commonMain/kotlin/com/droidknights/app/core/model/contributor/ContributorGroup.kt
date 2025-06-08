cefc910ab17fa6a415989653e9470ccfc261be95
haeti-dev <t9h9@soongsil.ac.kr>
2025-06-09 02:03:29 +0900

    [droidknights#508] core:model:model-contributor 모듈 생성 및 필요한 모델 생성

---
+++ core/model/model-contributor/src/commonMain/kotlin/com/droidknights/app/core/model/contributor/ContributorGroup.kt
@@ -0,0 +1,6 @@
+package com.droidknights.app.core.model.contributor
+
+data class ContributorGroup(
+    val year: Int,
+    val contributors: List<Contributor>,
+)
---
+++ core/model/model-contributor/src/commonMain/kotlin/com/droidknights/app/core/model/contributor/ContributorWithYears.kt
@@ -0,0 +1,6 @@
+package com.droidknights.app.core.model.contributor
+
+data class ContributorWithYears(
+    val id: Long,
+    val years: List<Int>,
+)
