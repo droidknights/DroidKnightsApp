5d867d44c113bb3a9e2a60a8f8ea12efe831a00c
easternkite <easternkite7@gmail.com>
2025-06-09 01:42:45 +0900

    (FEAT)[droidknights#511] bookmark 모듈 의존성 추가

--- feature/bookmark/build.gradle.kts
+++ feature/bookmark/build.gradle.kts
@@ -2,4 +2,15 @@ plugins {
     id("droidknights.feature")
 }
 
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(projects.core.domain.domainSession)
+            implementation(projects.core.domain.domainSessionApi)
+            implementation(projects.core.model.modelSession)
+            implementation(libs.kotlinx.immutable)
+        }
+    }
+}
+
 android.namespace = "com.droidknights.app.feature.bookmark"
