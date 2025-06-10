4a58611c5b5fe0f28e72ea2085962c67dcff4b0a
sayyyho <323psh@naver.com>
2025-06-10 21:17:40 +0900

    feature/#461: AppBar 구현

--- feature/main/src/wasmJsMain/kotlin/com/droidknights/app/feature/main/components/MobileAppBar.kt
+++ feature/main/src/wasmJsMain/kotlin/com/droidknights/app/feature/main/components/MobileAppBar.kt
@@ -2,45 +2,35 @@ package com.droidknights.app.feature.main.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.droidknights.app.core.designsystem.components.Text
 import com.droidknights.app.core.designsystem.theme.KnightsTheme
-import com.droidknights.app.feature.main.model.ProjectBranch
-import com.droidknights.app.feature.main.model.branches2025
 
 @Composable
-internal fun AppBar(
-    onProjectBranchClick: (ProjectBranch) -> Unit,
+internal fun MobileAppBar(
     modifier: Modifier = Modifier,
 ) {
-    Row(
+    FlowRow(
         modifier = modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically
+        verticalArrangement = Arrangement.Center
     ) {
         Column {
             Text(
                 text = "2025 Conference APP",
-                style = KnightsTheme.typography.displayMediumEB,
+                style = KnightsTheme.typography.headlineMediumB,
             )
             Spacer(modifier = Modifier.height(8.dp))
             Text(
                 text = "드로이드나이츠 앱의 컨트리뷰터가 되어주세요!",
-                style = KnightsTheme.typography.headlineLargeSB,
+                style = KnightsTheme.typography.titleMediumB,
             )
         }
-
-
-        ProjectBranchButtons(
-            onClick = onProjectBranchClick,
-            branches = branches2025,
-        )
     }
 }
