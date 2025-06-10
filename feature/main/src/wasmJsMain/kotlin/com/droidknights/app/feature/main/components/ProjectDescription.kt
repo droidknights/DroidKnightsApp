6309e3375bb5a90c3a9aa1c3818b476ad680bffb
sayyyho <323psh@naver.com>
2025-06-10 21:19:39 +0900

    fix/#461: ProjectDescription 수정

--- feature/main/src/wasmJsMain/kotlin/com/droidknights/app/feature/main/components/ProjectDescription.kt
+++ feature/main/src/wasmJsMain/kotlin/com/droidknights/app/feature/main/components/ProjectDescription.kt
@@ -1,63 +1,53 @@
 package com.droidknights.app.feature.main.components
 
-import androidx.compose.foundation.layout.Arrangement
+import Sponsors
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.droidknights.app.core.designsystem.components.Text
+import com.droidknights.app.core.designsystem.components.HorizontalDivider
 import com.droidknights.app.core.designsystem.theme.KnightsTheme
-import com.droidknights.app.core.designsystem.theme.LocalContentColor
 import com.droidknights.app.feature.main.model.Contributor
-import com.droidknights.app.feature.main.model.ProjectBranch
-import com.droidknights.app.feature.main.model.branches2025
 import com.droidknights.app.feature.main.model.contributors2023
 import com.droidknights.app.feature.main.model.contributors2024
+import com.droidknights.app.feature.main.model.sponsors2025
 
 @Composable
 internal fun ProjectDescription(
-    onProjectBranchClick: (ProjectBranch) -> Unit,
     onContributorClick: (Contributor) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = modifier.padding(vertical = 100.dp),
     ) {
-        Text(
-            text = "드로이드나이츠 앱의\n컨트리뷰터가 되어주세요!",
-            style = KnightsTheme.typography.displayMediumEB,
+        Contributors(
+            title = "2025",
+            contributors = contributors2024,
+            onClick = onContributorClick,
         )
+        Spacer(modifier = Modifier.height(55.dp))
 
-        Spacer(modifier = Modifier.height(8.dp))
-
-        ProjectBranchButtons(
-            onClick = onProjectBranchClick,
-            branches = branches2025,
+        Sponsors(
+            title = "후원자",
+            sponsors = sponsors2025
         )
+        Spacer(modifier = Modifier.height(58.dp))
 
-        Spacer(modifier = Modifier.height(48.dp))
-
+        HorizontalDivider(color = KnightsTheme.colorScheme.borderColor, thickness = 1.dp)
+        Spacer(modifier = Modifier.height(90.dp))
         Contributors(
             title = "2024",
             contributors = contributors2024,
             onClick = onContributorClick,
         )
-
+        Spacer(modifier = Modifier.height(40.dp))
         Contributors(
             title = "2023",
             contributors = contributors2023,
             onClick = onContributorClick,
-        )
-
-        Spacer(modifier = Modifier.height(48.dp))
-
-        Text(
-            text = "* 이 페이지는 Compose Multiplatform으로 만들어졌습니다. 웹에서 드로이드나이츠 앱을 미리 체험 해보세요!",
-            style = KnightsTheme.typography.bodyLargeR,
-            color = LocalContentColor.current.copy(alpha = 0.5F),
         )
     }
 }
